83c469529466f3284be76a181bad96d28bc91fe9
Bryan Van de Ven <bryanv@anaconda.com>
2018-03-23 11:24:50 -0700

    checkpoint

--- bokeh/plotting/figure.py
+++ bokeh/plotting/figure.py
@@ -331,6 +331,21 @@ Examples:
 
 """)
 
+    hex_tile = _glyph_function(glyphs.HexTile, """
+Examples:
+
+    .. bokeh-plot::
+        :source-position: above
+
+        from bokeh.plotting import figure, output_file, show
+
+        plot = figure(plot_width=300, plot_height=300, match_aspect=True)
+        plot.hex_tile(r=[0, 0, 1], q=[1, 2, 2], fill_color="#74ADD1")
+
+        show(plot)
+
+""")
+
     image = _glyph_function(glyphs.Image, """
 .. note::
     If both ``palette`` and ``color_mapper`` are passed, a ``ValueError``
